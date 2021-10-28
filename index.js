b47ef828b7356a5df21cd005f6a3c1a53851c59f
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2021-10-28 12:03:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Realm = require("realm");
 
 
+// Realm Schema Definition
 const Car = {
   name: "Car",
   properties: {
@@ -11,7 +12,16 @@ const Car = {
 };
 
 
-// Open a local realm file with a particular path & predefined Car schema
+// Realm Object Cahange Listener
+function listener(vehicle, changes) {
+  console.log(`${changes.changedProperties.length} properties have been changed:`);
+  changes.changedProperties.forEach(prop => {
+      console.log("Modified Attributes: " + `${prop}: ${vehicle[prop]}`);
+    });
+}
+
+
+// Open a local realm file with default path & predefined Car schema
 try {
   const realm = Realm.open({
     schema: [Car],
@@ -19,6 +29,9 @@ try {
     realm.write(() => {
       car = realm.create("Car", {_id: 1, make: "BMW", model: "X5", miles: 0});
     });
+    return car;
+  }).then((car) => {
+    car.addListener(listener);
   });
 } catch (err) {
   console.error("Failed to open the realm", err.message);
