cf8661ea059c49236dceb071daf7c4e4be26f22d
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2021-10-28 12:22:00 +0200

    Updated Readme

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-Vehicle-Shadow
+# Device-Shadow Realm Example
 
-https://docs.mongodb.com/realm/sdk/node/
+With MongoDB Realm's change notifications, device updates can easily be tracked and automtatic notifications including only the attributes changed can be created and as an example be sent to the backend via MQTT or completely out of the box synchronized bidirectionally with Realm Sync!
 
-TypeScript
-https://github.com/Microsoft/TypeScript-Node-Starter#typescript--node
+## Prerequisits:
+* [Install npm / Realm SDK](https://docs.mongodb.com/realm/sdk/node/)
+* [Install Realm Studio](https://docs.mongodb.com/realm/studio/)
 
-tsc --init
+
+## Rund The Demo
+
+![Source Code](/media/0_VisualStudio_SourceCode.png)
+![Source Code](/media/1_RealmStudio_Empty.png)
+![Source Code](/media/2_Shell_StartApp.png)
+![Source Code](/media/3_RealmStudio_CarCreated.png)
+![Source Code](/media/4_RealmStudio_CarModified.png)
+![Source Code](/media/5_Shell_ChangeModifications.png)
 
 
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Realm = require("realm");
 
-
 // Realm Schema Definition
 const Car = {
   name: "Car",
@@ -11,28 +10,35 @@ const Car = {
   },
 };
 
-
 // Realm Object Cahange Listener
 function listener(vehicle, changes) {
-  console.log(`${changes.changedProperties.length} properties have been changed:`);
-  changes.changedProperties.forEach(prop => {
-      console.log("Modified Attributes: " + `${prop}: ${vehicle[prop]}`);
-    });
+  console.log(
+    `${changes.changedProperties.length} properties have been changed:`
+  );
+  changes.changedProperties.forEach((prop) => {
+    console.log("Modified Attributes: " + `${prop}: ${vehicle[prop]}`);
+  });
 }
-
 
 // Open a local realm file with default path & predefined Car schema
 try {
   const realm = Realm.open({
     schema: [Car],
-  }).then(realm => {
-    realm.write(() => {
-      car = realm.create("Car", {_id: 1, make: "BMW", model: "X5", miles: 0});
+  })
+    .then((realm) => {
+      realm.write(() => {
+        car = realm.create("Car", {
+          _id: 1,
+          make: "BMW",
+          model: "X5",
+          miles: 0,
+        });
+      });
+      return car;
+    })
+    .then((car) => {
+      car.addListener(listener);
     });
-    return car;
-  }).then((car) => {
-    car.addListener(listener);
-  });
 } catch (err) {
   console.error("Failed to open the realm", err.message);
 }
