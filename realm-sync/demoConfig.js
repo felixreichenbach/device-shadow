51937f644c93d224ca9b0400b9c7d88b378baf2a
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2021-11-04 17:06:06 +0100

    Create demoConfig.js

---
+++ realm-sync/demoConfig.js
@@ -0,0 +1,15 @@
+
+// Provide the Realm App ID
+exports.RealmAppID = "<< YOUR APP ID >>";
+
+// Provide the configured customer profile
+exports.CProfile = {
+    username: "<< YOUR Customer Username >>",
+    password: "<< YOUR Customer Password >>"
+}
+
+// Provide the configured vehicle profile
+exports.VProfile = {
+    username: "<< YOUR Vehicle Username >>",
+    password: "<< YOUR Vehicle Password >>"
+}
